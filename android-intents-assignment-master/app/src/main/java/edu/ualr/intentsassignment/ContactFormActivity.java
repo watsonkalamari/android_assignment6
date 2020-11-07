a3ec2e4ae827f343c411370eba9cd999bbda9b05
watsonkalamari <klwatson3@ualr.edu>
2020-11-06 18:04:50 -0600

    Work in Progress

--- android-intents-assignment-master/app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
+++ android-intents-assignment-master/app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
@@ -1,9 +1,18 @@
 package edu.ualr.intentsassignment;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactFormActivity extends AppCompatActivity {
     // TODO 01. Create a new layout file to define the GUI elements of the ContactFormActivity.
     // TODO 02. Define the basic skeleton of the ContactFormActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
     // TODO 06. Create a new method that reads the values in the several EditText elements of the layout and then uses the Contact class to send those data to ContactInfoActivity
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.contact_form_activity);
+    }
+
 }
